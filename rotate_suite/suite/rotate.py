8e446079cdc12d955c42b45fded4e7b62f01cc0f
Sahand <sahand.rezaei.sh@gmail.com>
2022-04-25 23:27:07 -0400

    Add some basic shapes

--- rotate_suite/suite/rotate.py
+++ rotate_suite/suite/rotate.py
@@ -15,18 +15,23 @@ _DEFAULT_TIME_LIMIT = 20
 _ANGLE_BOUND = 8
 _COSINE_BOUND = np.cos(np.deg2rad(_ANGLE_BOUND))
 SUITE = containers.TaggedTasks()
+OBJECT_TYPES = ['box', 'capsule', 'ellipsoid', 'cylinder']
 
 
-def get_model_and_assets():
+# TODO: Add more randomization to the environment
+
+
+def get_model_and_assets(object_type):
     """Returns a tuple containing the model XML string and a dict of assets."""
-    return common.read_model('assets/box.xml'), common.ASSETS
+    return common.read_model(f'assets/{object_type}.xml'), common.ASSETS
 
 
 @SUITE.add('rotate')
-def easy(time_limit=_DEFAULT_TIME_LIMIT, random=None,
+def easy(time_limit=_DEFAULT_TIME_LIMIT, object_type='box', random=None,
          environment_kwargs=None):
-    """Returns pendulum swingup task ."""
-    physics = Physics.from_xml_string(*get_model_and_assets())
+    """Returns rotate easy task ."""
+    assert object_type in OBJECT_TYPES, f"Available object types are {OBJECT_TYPES}"
+    physics = Physics.from_xml_string(*get_model_and_assets(object_type))
     task = Rotate(random=random)
     environment_kwargs = environment_kwargs or {}
     return control.Environment(
@@ -34,7 +39,7 @@ def easy(time_limit=_DEFAULT_TIME_LIMIT, random=None,
 
 
 class Physics(mujoco.Physics):
-    """Physics simulation with additional features for the Pendulum domain."""
+    """Physics simulation with additional features for the Rotate domain."""
 
     def orientation(self):
         """Returns the rotation matrix of the base."""
@@ -71,20 +76,21 @@ class Rotate(base.Task):
     def get_observation(self, physics):
         """Returns an observation.
 
-        Observations are states concatenating pole orientation and angular velocity
+        Observations are states concatenating the base orientation
         and pixels from fixed camera.
 
         Args:
-          physics: An instance of `physics`, Pendulum physics.
+          physics: An instance of `physics`, Rotate physics.
 
         Returns:
           A `dict` of observation.
         """
+        # TODO
         obs = collections.OrderedDict()
         obs['orientation'] = physics.orientation()
-        # obs['velocity'] = physics.angular_velocity()
         return obs
 
     def get_reward(self, physics):
+        # TODO
         return 0.
         # return rewards.tolerance(physics.pole_vertical(), (_COSINE_BOUND, 1))
